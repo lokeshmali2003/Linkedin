674c7899c044330d14e15d457f65d81bc18927ba
Lokesh Mali <malilokesh311@gmail.com>
2025-12-06 18:14:08 +0530

    process

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+const uploadOnCloudinary = async (filePath) =>{
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY , 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    try{
+      if(!filePath){
+          return null
+      }
+          // Upload an image
+     const uploadResult = await cloudinary.uploader.upload( filePath )
+
+    }catch(error){
+
+    }
+}
