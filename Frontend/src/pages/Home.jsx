69978569cf003268fb3c3b5ef8f33c10d04db787
Lokesh Mali <malilokesh311@gmail.com>
2025-12-07 17:35:06 +0530

    process

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from '../components/Nav.jsx'
 import dp from '../assets/profileBlanck.webp'
+import { FiPlus } from "react-icons/fi";
+import { IoCameraOutline } from "react-icons/io5";
+import { UserDataContext } from '../context/userContext.jsx';
 function Home() {
+    const { userData, setUserData } = useContext(UserDataContext)
   return (
     <div className='w-full min-h-[100vh] bg-[#f0efe7] pt-[100px] flex items-start justify-center gap-[20px] px-[20px] flex-col lg:flex-row'>
       <Nav />
-      <div className='w-full lg:w-[25%] rounded-lg min-h-[200px] bg-white shadow-lg p-[10px]' >
+      <div className='w-full lg:w-[25%] rounded-lg min-h-[200px] bg-white shadow-lg p-[10px] relative' >
         <div className='w-[100%] h-[100px] bg-gray-400 rounded overflow-hidden flex items-center justify-center'>
           <img src='' alt='' className='w-full' />
+           <IoCameraOutline className='absolute right-[20px] top-[20px] w-[25px] h-[25px] text-gray-800 cursor-pointer'  />
         </div>
-        <div className='w-[70px] h-[70px] rounded-full overflow-hidden items-center justify-center relative top-[-45px] left-[30px]' >
+        <div className='w-[70px] h-[70px] rounded-full overflow-hidden items-center justify-center absolute top-[65px] left-[35px] cursor-pointer' >
           <img src={dp} alt='' className='h-full' />
+          
         </div>
+         <div className="w-[20px] h-[20px]  bg-[#17c1ff] absolute top-[105px] left-[90px] rounded-full flex justify-center items-center">
+            <FiPlus className='text-white' />
+          </div>
+          <div className="mt-[25px] pl-[20px]">
+            <div>{`${userData?.user?.firstName} ${userData?.user?.lastName}`}</div>
+            
+          </div>
       </div>
 
       <div className='w-full lg:w-[50%] min-h-[200px] bg-white shadow-lg'></div>
